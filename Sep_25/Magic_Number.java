ef2bf64abd443fe4084bc8f5bf8ad45da6d30043
HITESH-235 <codonstream.72@gmail.com>
2025-09-26 19:08:07 +0530

    26 Sep 2025

---
+++ Sep_25/Magic_Number.java
@@ -0,0 +1,25 @@
+package CP_Club_SST.Sep_25;
+
+import java.util.Scanner;
+
+public class Magic_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(magicNumber(n));
+        sc.close();
+    }
+
+
+public static int magicNumber(int n) {
+    if (n<10) { return n==1? 1 : 0; }
+    int sum = 0;
+    while (n!=0) {
+        sum += n%10;
+        n /= 10;
+    }
+    return magicNumber(sum);
+}
+}
+// 83593 => 28 => 10 => 1
+// 10000 => 1
